66ae205f2e6901e49c6b6bd17f4e6850d7c8568e
Salavat F <salmrproperfahr@mail.ru>
2022-08-25 22:38:14 +0300

    create rules components

---
+++ src/components/Rules/Rules.jsx
@@ -0,0 +1,12 @@
+const Rules = () => {
+    return (
+        <section className="rules" id="rules">
+            <div className="container">
+                <h2 className="page__title">NOTEIKUMI</h2>
+                <div className="rules__inner"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Rules;
