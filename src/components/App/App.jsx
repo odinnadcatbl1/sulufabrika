6a4917a237bc9311e3e5c0e961da26612edb87af
Salavat F <salmrproperfahr@mail.ru>
2022-08-26 13:46:51 +0300

    done responsive info block

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
             <Header />
             <Intro />
             <Contacts />
+            <InfoBlock />
             {/* <Contacts />
             <InfoBlock />
             <Rules />
