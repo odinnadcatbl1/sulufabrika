a1645a7d7a2d4da02550ea8a7a48956f8afd6098
Salavat F <salmrproperfahr@mail.ru>
2022-10-16 11:21:57 +0300

    fixed gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,11 +2,14 @@ import Slider from "react-slick";
 
 import photo1 from "../../assets/gallery/1.jpg";
 import photo2 from "../../assets/gallery/2.jpg";
-import photo3 from "../../assets/gallery/1.jpg";
-import photo4 from "../../assets/gallery/2.jpg";
-import photo5 from "../../assets/gallery/1.jpg";
+import photo3 from "../../assets/gallery/3.jpg";
+import photo4 from "../../assets/gallery/4.jpg";
+import photo5 from "../../assets/gallery/5.jpg";
+import photo6 from "../../assets/gallery/6.jpg";
+import photo7 from "../../assets/gallery/7.jpg";
+import photo8 from "../../assets/gallery/8.jpg";
 
-const photos = [photo1, photo2, photo3, photo4, photo5];
+const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8];
 
 const Gallery = () => {
     const sliderSetting = {
